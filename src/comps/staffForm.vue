6e176de79fa4cf540e3b489e3dc71085af9ed871
opteacher <opteacher@hotmail.com>
2021-03-29 00:05:54 +0800

    页面完成

---
+++ src/comps/staffForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <mt-field label="姓名" placeholder="请输入真实姓名" v-model="name"/>
+    <mt-field label="身份证" placeholder="请输入身份证号码" type="number" v-model="id"/>
+    <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"/>
+    <mt-field label="户籍地址" placeholder="请输入户籍地址" v-model="hhAddress"/>
+    <mt-field label="居住地址" placeholder="请输入居住地址" v-model="lvAddress"/>
+    <mt-cell title="所在单位" to="//github.com" is-link value="请选择所在单位"/>
+    <div class="w-100 fixed-bottom mb-1pr">
+      <mt-button class="mlr-1pr" type="primary" style="width: 98vw">提交</mt-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: "",
+        id: "",
+        phone: "",
+        hhAddress: "",
+        lvAddress: "",
+        workComp: ""
+      }
+    },
+    async created() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style type="text/scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import VueRouter from "vue-router"
 import VueAxios from "vue-axios"
 import axios from "axios"
 import routes from "./routes"
+import "./styles/common.scss"
 import "mint-ui/lib/style.min.css"
-import { Field, Button } from 'mint-ui'
+import { Field, Button, Navbar, TabItem, TabContainer, TabContainerItem, Cell } from 'mint-ui'
 
 Vue.component(Field.name, Field)
 Vue.component(Button.name, Button)
+Vue.component(Navbar.name, Navbar)
+Vue.component(TabItem.name, TabItem)
+Vue.component(TabContainer.name, TabContainer)
+Vue.component(TabContainerItem.name, TabContainerItem)
+Vue.component(Cell.name, Cell)
 
 Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
