bad8c919e26dfc45ffd1c4263794662ce7abe4bb
opteacher <opteacher@hotmail.com>
2021-04-14 18:26:49 +0800

    注释掉短信验证码功能，等短信验证服务办下来了再开

--- src/comps/connectForm.vue
+++ src/comps/connectForm.vue
@@ -2,11 +2,12 @@
   <div>
     <mt-field id="phone" label="手机号" placeholder="请输入手机号" type="tel" v-model="form.phone"
       data-container="body" data-toggle="popover" data-trigger="manual" data-placement="bottom"/>
-    <mt-field label="验证码" placeholder="请输入验证码" v-model="vcode">
+    <!-- @_@：先注释掉，等短信业务办下来了再开 -->
+    <!-- <mt-field label="验证码" placeholder="请输入验证码" v-model="vcode">
       <mt-button :disabled="cdTime !== 0" size="small" @click.native="onSendVcodeClick">
         {{cdTime === 0 ? "发送验证码" : `${cdTime}s后重新发送`}}
       </mt-button>
-    </mt-field>
+    </mt-field> -->
   </div>
 </template>
 
